1f780d76fd6c043b78ff6b7d80389431a819f846
lenovo <lenovo@DESKTOP-4DR0A54.mshome.net>
2023-10-26 16:53:22 +0700

    "Added a simple Java program"

---
+++ Modul3_Tugas/src/modul3_tugas/Modul3_Tugas.java
@@ -0,0 +1,13 @@
+package modul3_tugas;
+
+public class Modul3_Tugas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello Git");
+    }
+    
+}
